63501641f95837c73f3a6cd0715eb4923222ce53
crabshank <44329154+crabshank@users.noreply.github.com>
2021-03-08 01:57:08 +0000

    Add files via upload

--- background.js
+++ background.js
@@ -1,28 +1,33 @@
-let contexts = ["image"];
+try {
+  let contexts = ["image"];
 chrome.contextMenus.create({
 	"title": "Yandex reverse image search (similar)",
 	"contexts": contexts,
-	"onclick": function(info, tab) {
-		//let to_url=(typeof info.linkUrl === 'undefined')?info.srcUrl:info.linkUrl;
+	"id":"sim"
+})
+
+chrome.contextMenus.create({
+	"title": "Yandex reverse image search (search)",
+	"contexts": contexts,
+	"id":"srch"
+});
+
+chrome.contextMenus.onClicked.addListener((info,tab) => {
+	if(info.menuItemId=="sim"){
 			chrome.tabs.create({
 				"url": 'https://yandex.com/images/search?url='+info.srcUrl+'&rpt=imagelike',
 				"windowId": tab.windowId,
 				"active": false
 			}, function(tab) {});
-
-	}
-});
-
-chrome.contextMenus.create({
-	"title": "Yandex reverse image search (search)",
-	"contexts": contexts,
-	"onclick": function(info, tab) {
-		//let to_url=(typeof info.linkUrl === 'undefined')?info.srcUrl:info.linkUrl;
-			chrome.tabs.create({
+	}else if(info.menuItemId=="srch"){
+		chrome.tabs.create({
 				"url": 'https://yandex.com/images/search?url='+info.srcUrl+'&rpt=imageview',
 				"windowId": tab.windowId,
 				"active": false
 			}, function(tab) {});
-
 	}
+
 });
+} catch (e) {
+  console.error(e);
+}
